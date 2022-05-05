598fa74d3339bcdc65f8dfb3288e46601cb4b77a
jules-deschamps-dev <jules.deschamps.dev@gmail.com>
2022-05-05 11:02:19 +0200

    addSomePackage

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer  = require('multer');
 const path = require('path');
+const helmet = require("helmet");
+require('dotenv').config();
 
 const userRoutes = require('./routes/auth');
 const saucesRoutes = require('./routes/sauces');
@@ -30,6 +32,7 @@ app.use((req, res, next) => {
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', saucesRoutes);
+app.use(helmet());
 
 
 
